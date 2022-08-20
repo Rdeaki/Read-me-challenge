7bb974caa03d471b9b0042e94d94e6a82a94846f
rick Deakins <rdeakins96@gmail.com>
2022-08-20 00:24:02 -0400

    lsit placeholder

--- script.js
+++ script.js
@@ -8,28 +8,28 @@ inquirer.Prompt(
     [
         {
             type: 'input',
-            message:"What's the project title?(REQUIRED)",
+            message:"What's the project title?",
             name:'title',
             //validate property to check that the user provided a value
             validate: (value)=>{ if (value){return true} else {return 'add a value to continue'}}
         },
         {
             type: 'input',
-            message:'what is the description of the project?(REQUIRED)',
+            message:'what is the description of the project?',
+            name:'description',
+            validate: (value)=>{ if (value){return true} else {return 'add a value to continue'}}
+
+        },
+        {
+            type: 'input',
+            message:'Provide details about installation of your app?',
             name:'installation',
             validate: (value)=>{ if (value){return true} else {return 'add a value to continue'}}
 
         },
         {
             type: 'input',
-            message:'provide details about installation of your app?',
-            name:'installation',
-            validate: (value)=>{ if (value){return true} else {return 'add a value to continue'}}
-
-        },
-        {
-            type: 'input',
-            message:'provide examples of uses for the application?',
+            message:'Provide examples of uses for the application?',
             name:'usage',
             validate: (value)=>{ if (value){return true} else {return 'add a value to continue'}}
 
@@ -37,7 +37,7 @@ inquirer.Prompt(
         {
             //license !!!come back to later!!!
             type: 'List',
-            message:'Licensing information',
+            message:'Provide licensing information',
             name:'License',
             choices: [filler, filler, filler],
             validate: (value)=>{ if (value){return true} else {return 'add a value to continue'}}
@@ -45,17 +45,33 @@ inquirer.Prompt(
         },
         {
             type: 'input',
-            message:'GitHub username',
-            name:'Git',
+            message:'Provide a list of contributers',
+            name:'contributions',
             validate: (value)=>{ if (value){return true} else {return 'add a value to continue'}}
 
         },
         {
             type: 'input',
-            message:'email',
+            message:'Provide GitHub username',
+            name:'git',
+            validate: (value)=>{ if (value){return true} else {return 'add a value to continue'}}
+
+        },
+        {
+            type: 'input',
+            message:'Provide email',
             name:'email',
             validate: (value)=>{ if (value){return true} else {return 'add a value to continue'}}
 
-        },        
+        },   
     ]
-)
+).then(({
+    title,
+    description,
+    installation,
+    usage,
+    license,
+    contributions,
+    git,
+    email,
+}))
